82b70f00e94e90630046d24ebf51a70864ad1c6f
Simran <simrangupta45300@gmail.com>
2026-08-03 01:26:39 +0530

    first commit

---
+++ Web/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import { Home } from "./Pages";
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  );
+}
+
+export default App;
